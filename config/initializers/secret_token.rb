2cc45297306fc1f8f86f0255b58e188d04db88b0
samantha <samantha.grimm@gmail.com>
2012-11-26 10:31:53 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mapmystyle::Application.config.secret_token = '051e8a11f9afd3f847462c6fd0627ac882e667b69a5e4ab87555282f8e732d8f4d8b58946f23417af0cb940a74a86ef5f92bc6569a36269b605921623bf086f5'
