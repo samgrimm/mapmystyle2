b7817ce51d2133bc832a27c7b03ea0fb2a7a4f97
samantha <samantha.grimm@gmail.com>
2012-11-26 12:33:37 -0600

    Finished initial pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+	def title
+		base_title = "Map My Style - Find your personal style"
+	if @title.nil?
+		base_title
+	else
+		"#{base_title} | #{@title}"
+	end
+
+end
 end
